a18951a105bb5049a7ad6eb6535890e3e1a16653
demuu <preasim.kr@gmail.com>
2024-01-08 21:12:28 +0900

    Update demuu.js

--- portfolio-page/DemuuPage/demuu.js
+++ portfolio-page/DemuuPage/demuu.js
@@ -5,7 +5,7 @@ const windowWidth = window.innerWidth;
 const toggleData = [
   { className: '.toggle-code-1', classBtnName: '.tcb1', trClass: '.tr1', tdClass: '.td1', height: '3370px', isOpen: false },
   { className: '.toggle-code-2', classBtnName: '.tcb2', trClass: '.tr2', tdClass: '.td2', height: '266px', isOpen: false },
-  { className: '.toggle-code-3', classBtnName: '.tcb3', trClass: '.tr3', tdClass: '.td3', height: '3195px', isOpen: false }
+  { className: '.toggle-code-3', classBtnName: '.tcb3', trClass: '.tr3', tdClass: '.td3', height: '3123px', isOpen: false }
 ];
 
 toggleData.forEach(data => {
