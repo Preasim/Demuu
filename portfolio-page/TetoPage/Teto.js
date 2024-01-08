e0e676139943b4709249536af303bfd7a55dc4b5
demuu <preasim.kr@gmail.com>
2024-01-08 15:59:06 +0900

    Update Teto.js

--- portfolio-page/TetoPage/Teto.js
+++ portfolio-page/TetoPage/Teto.js
@@ -3,9 +3,9 @@ import { toggleCode } from '../uncoverPage/toggleCode.js';
 const windowWidth = window.innerWidth;
 
 const toggleData = [
-  { className: '.toggle-code-4', classBtnName: '.tcb4', trClass: '.tr4', tdClass: '.td4', height: '1425px', isOpen: false },
-  { className: '.toggle-code-5', classBtnName: '.tcb5', trClass: '.tr5', tdClass: '.td5', height: '520px', isOpen: false },
-  { className: '.toggle-code-6', classBtnName: '.tcb6', trClass: '.tr6', tdClass: '.td6', height: '8460px', isOpen: false }
+  { className: '.toggle-code-1', classBtnName: '.tcb1', trClass: '.tr1', tdClass: '.td1', height: '1425px', isOpen: false },
+  { className: '.toggle-code-2', classBtnName: '.tcb2', trClass: '.tr2', tdClass: '.td2', height: '520px', isOpen: false },
+  { className: '.toggle-code-3', classBtnName: '.tcb3', trClass: '.tr3', tdClass: '.td3', height: '8460px', isOpen: false }
 ];
 
 toggleData.forEach(data => {
