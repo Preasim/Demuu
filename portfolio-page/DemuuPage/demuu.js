ca8e7ce4c14c697f9efdc0cc50e0781a704f303b
demuu <preasim.kr@gmail.com>
2024-01-08 21:07:23 +0900

    Update demuu.js

--- portfolio-page/DemuuPage/demuu.js
+++ portfolio-page/DemuuPage/demuu.js
@@ -3,9 +3,9 @@ import { toggleCode } from '../uncoverPage/toggleCode.js';
 const windowWidth = window.innerWidth;
 
 const toggleData = [
-  { className: '.toggle-code-1', classBtnName: '.tcb1', trClass: '.tr1', tdClass: '.td1', height: '1425px', isOpen: false },
-  { className: '.toggle-code-2', classBtnName: '.tcb2', trClass: '.tr2', tdClass: '.td2', height: '520px', isOpen: false },
-  { className: '.toggle-code-3', classBtnName: '.tcb3', trClass: '.tr3', tdClass: '.td3', height: '8460px', isOpen: false }
+  { className: '.toggle-code-1', classBtnName: '.tcb1', trClass: '.tr1', tdClass: '.td1', height: '3370px', isOpen: false },
+  { className: '.toggle-code-2', classBtnName: '.tcb2', trClass: '.tr2', tdClass: '.td2', height: '266px', isOpen: false },
+  { className: '.toggle-code-3', classBtnName: '.tcb3', trClass: '.tr3', tdClass: '.td3', height: '3195px', isOpen: false }
 ];
 
 toggleData.forEach(data => {
