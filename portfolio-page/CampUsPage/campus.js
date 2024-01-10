c0d4acd217281f4d89a243c33d4865bf743dc3a6
demuu <preasim.kr@gmail.com>
2024-01-10 10:30:57 +0900

    Update campus.js

--- portfolio-page/CampUsPage/campus.js
+++ portfolio-page/CampUsPage/campus.js
@@ -9,9 +9,3 @@ const toggleData = [
 toggleData.forEach(data => {
   toggleCode(data, windowWidth);
 });
-
-window.addEventListener('resize', function() {
-  toggleData.forEach(data => {
-    toggleCode(data, window.innerWidth);
-  });
-});
